6799946219dab91609caf883b405f2792cb2918e
Piotr <Piotr@LENOVOZ70>
2015-12-02 19:17:52 +0100

    TestCard

--- src/test/java/tp/poker/texas/holdem/CardTest.java
+++ src/test/java/tp/poker/texas/holdem/CardTest.java
@@ -5,9 +5,40 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class CardTest extends TestCase {
+	private Card karta;
+	
+	public void setUp() {
+		karta = new Card(0, 0);
+	}
+	
+	public void tearDown() {
+		karta = null;
+	}
+	
+	
+	//Test czy obiekt klasy Card jest tworzony.
 	@Test
 	public void testKarty() {
-		Card karta = new Card(2,2);
 		assertNotNull(karta);
+	}
+	
+	/* Testy metod klasy Card */
+	
+	//Test metody String nazwaKarty().
+	@Test
+	public void testMtdNazwaKarty() {
+		assertEquals("dwojka trefl", karta.nazwaKarty());
+	}
+	
+	//Test metody int getKolor()
+	@Test
+	public void testMtdGetKolor() {
+		assertEquals(0, karta.getKolor());
+	}
+	
+	//Test metody int getNumFig()
+	@Test
+	public void testMtdGetNumFig() {
+		assertEquals(0, karta.getNumFig());
 	}
 }
