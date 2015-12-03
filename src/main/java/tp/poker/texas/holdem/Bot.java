d6ce0a8fe474eebd9095d54c51c529971a146fcc
pokertexasholdem2015mn <pokertexasholdem2015@gmail.com>
2015-12-03 16:53:57 +0100

    few methods

--- src/main/java/tp/poker/texas/holdem/Bot.java
+++ src/main/java/tp/poker/texas/holdem/Bot.java
@@ -1,5 +1,9 @@
 package tp.poker.texas.holdem;
 
 public class Bot extends Player {
+	
+	public int BotBetStrategy(Table stol){
+		
+	}
 
 }
--- src/main/java/tp/poker/texas/holdem/Player.java
+++ src/main/java/tp/poker/texas/holdem/Player.java
@@ -1,5 +1,25 @@
 package tp.poker.texas.holdem;
 
 public abstract class Player {
+	public Card reka[] = new Card[5];
+	int uklad = 0;
+	private int zetony = 0;
+	public abstract int BotBetStrategy(Table stol);
+	
+	public Card odbierzKarte(Card karta)
+	{
+		for (int i = 0; i < 5; i++) {
+			if (reka[i] == null) {
+				reka[i] = karta;
+				if(reka[4] != null)
+				{
+					//CardSort();
+					//rangaUkladu();
+				}
+				return null;
+			}
+		}
+		return karta;
+	}
 
 }
